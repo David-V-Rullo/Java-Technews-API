606e270625301a2c76a21256227f092e67623223
David Rullo <d.v.rullo@gmail.com>
2021-09-27 10:11:23 -0400

    Testing

---
+++ tech-news-java-api/src/main/java/com/technews/Variables.java
@@ -0,0 +1,12 @@
+package com.technews;
+
+public class Variables {
+    public static void main(String[] args){
+        String name = "Dave";
+        System.out.println(name);
+        int a = 10;
+        double b = 10.80;
+        double sum = a + b;
+        System.out.println(sum);
+    }
+}
